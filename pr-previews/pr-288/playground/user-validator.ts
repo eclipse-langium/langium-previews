e02ef9e6dca9bbe41d70cbeab66f6d02b509f289
emilkrebs <emilkrebs@users.noreply.github.com>
2025-11-19 12:24:04 +0000

    Deploy preview for PR 288 🛫

---
+++ pr-previews/pr-288/playground/user-validator.ts
@@ -0,0 +1,29 @@
+import { AstNode, AstNodeDescription, AstUtils, DefaultDocumentValidator, DefaultLinker, DiagnosticInfo, DocumentState, DocumentValidator, LangiumDocument, LinkingError, LinkingErrorData, ReferenceInfo, ValidationOptions } from "langium";
+import { LangiumServices } from "langium/lsp";
+import { Diagnostic } from "vscode-languageserver";
+
+export class PlaygroundValidator extends DefaultDocumentValidator {
+    constructor(services: LangiumServices) {
+        super(services);
+    }
+    protected override processLinkingErrors(document: LangiumDocument, diagnostics: Diagnostic[], _options: ValidationOptions): void {
+        for (const reference of document.references) {
+            const linkingError = reference.error;
+            if (linkingError) {
+                const info: DiagnosticInfo<AstNode, string> = {
+                    node: linkingError.info.container,
+                    range: reference.$refNode?.range,
+                    property: linkingError.info.property,
+                    index: linkingError.info.index,
+                    data: {
+                        code: DocumentValidator.LinkingError,
+                        containerType: linkingError.info.container.$type,
+                        property: linkingError.info.property,
+                        refText: linkingError.info.reference.$refText
+                    } satisfies LinkingErrorData
+                };
+                diagnostics.push(this.toDiagnostic('warning', `${linkingError.message}\nIn case you want to adjust the linking rules, please consult the learning section in the Langium documentation.`, info));
+            }
+        }
+    }
+};
